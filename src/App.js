d267d58f2f45bdcf382a13b1822446c36e9e300e
ghouili <walid.ghouili98@gmail.com>
2022-04-08 15:37:42 +0100

    routing + sidebar + navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { Link, Routes, Route } from 'react-router-dom';
+import { HiMenu } from 'react-icons/hi'
+import { HiMenuAlt2 } from 'react-icons/hi'
+import { useState } from 'react';
+
+import Dashboard from './Pages/Dashboard';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
 function App() {
+
+  const [side_show, setSide_show] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className='NavBar'>
+
+        <div className='menu_icon_container' >
+          {side_show?
+            <HiMenu className='menu_icon' onClick={() => setSide_show(!side_show)}/>
+          :
+            <HiMenuAlt2 className='menu_icon' onClick={() => setSide_show(!side_show)}/>
+          }
+        </div>
+
+        <div className='logo_contaciner'>
+          <h1>Logo App</h1>
+        </div>
+
+        <div className='links'>
+          <Link to='/' className='' >Home</Link>
+          <Link to='/about' >About</Link>
+          <Link to='/contact' >Contact</Link>
+        </div>
+
+        
       </header>
-    </div>
+      
+      <main>
+        <nav className={side_show ? 'SideBar active' : 'SideBar'}>
+          <Link to='/' className='' >Home</Link>
+          <Link to='/about' >About</Link>
+          <Link to='/contact' >Contact</Link>
+        </nav>
+
+        <div className={side_show ? 'Main_content active' : 'Main_content'}>
+          <Routes>
+            <Route path='/' element={< Dashboard />} />
+            <Route path='/about' element={< About />} />
+            <Route path='/contact' element={< Contact />} />
+          </Routes>
+        </div>
+      </main>
+
+    </>
   );
 }
 
